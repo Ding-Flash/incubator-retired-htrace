38a5cb27be876fdb2d974b49e198a84cb60fc7d9
wushuhan <982552668@qq.com>
2019-04-15 12:00:16 +0800

    fix TBucketSampler

--- htrace-core4/src/main/java/org/apache/htrace/core/TBucketSampler.java
+++ htrace-core4/src/main/java/org/apache/htrace/core/TBucketSampler.java
@@ -1,5 +1,8 @@
 package org.apache.htrace.core;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
@@ -8,44 +11,65 @@ import java.util.List;
 
 public class TBucketSampler extends Sampler {
     public static ConcurrentHashMap<String, List<Long>> records = new ConcurrentHashMap<>();
-    Long curTime = System.currentTimeMillis();
-    public final static String BUCKET_SIZE = "sampler.bucketSize";
-    public final static String INCREASE_STEP = "sampler.increaseStep";
-    private Long bktSize;
-    private Long incStep;
+    public static final String BUCKET_SIZE = "sampler.bucketSize";
+    public static final String INCREASE_STEP = "sampler.increaseStep";
+    public static Long bktSize;
+    public static Long incStep;
+//    public static final Log LOG = LogFactory.getLog("MyLog");
+
 
     public TBucketSampler(HTraceConfiguration conf) {
-        this.bktSize = Long.parseLong(conf.get(BUCKET_SIZE));
-        this.incStep = Long.parseLong(conf.get(INCREASE_STEP));
+        bktSize = Long.parseLong(conf.get(BUCKET_SIZE));
+        incStep = Long.parseLong(conf.get(INCREASE_STEP));
     }
 
-    private Long newtok(){
+    private static Long newtok(String description){
         Long newTime = System.currentTimeMillis();
-        Long tok = (newTime - curTime)/incStep;
+        List Func = records.get(description);
+        Long lastTime = (Long)Func.get(2);
+        Long tok = (newTime - lastTime)/incStep;
         if (tok>0){
-            curTime = newTime;
+            Func.set(2,newTime);
+            records.put(description,Func);
         }
 
         return tok;
     }
-    private boolean update(String description){
+    private static synchronized boolean update(String description){
         if(records.get(description)==null) {
             List<Long> TandN = new ArrayList<>();
-            TandN.add(49L);
+//          Data Format:token,number,last timestamp
+            TandN.add(bktSize-1);
             TandN.add(1L);
+            TandN.add(System.currentTimeMillis());
             records.put(description, TandN);
+//            LOG.info("new trace \""+description+"\" added"+"with tokens left "+TandN.get(0)+" and counting "+TandN.get(1));
             return true;
+
         }
 
         List<Long> f = records.get(description);
         if(f.get(0) > 0){
-            Long tok = newtok();
+            Long tok = newtok(description);
             f.set(0,f.get(0)+tok>bktSize?bktSize-1:f.get(0)+tok-1);
             f.set(1,f.get(1)+1);
-            if(f.get(1)%100 == 0){
-                bktSize = f.get(1)/10;
-            }
+            records.put(description,f);
+//            if(f.get(1)%100 == 0){
+//                bktSize = f.get(1)/10;
+//            }
+//            LOG.info("trace \""+description+"\" sampled "+"with token left "+f.get(0)+" and counting "+f.get(1));
             return true;
+        }
+//        LOG.info("trace \""+description+"\" not sampled "+"with token left "+f.get(0)+" and counting "+f.get(1));
+        if(f.get(0) == 0){
+            Long tok = newtok(description);
+            f.set(0,tok);
+            records.put(description,f);
+//            if(f.get(1)%100 == 0){
+//                bktSize = f.get(1)/10;
+//            }
+//            LOG.info("trace \""+description+"\" sampled "+"with token left "+f.get(0)+" and counting "+f.get(1));
+            return false;
         }
         return false;
     }
